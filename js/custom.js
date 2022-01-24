0f04839ac66ea10a8b134fad8599427b5a674805
hasanrafi69 <hasanrafi69@gmail.com>
2022-01-24 17:11:34 +0600

    fix tost problem

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,24 @@
 /* 
-* Author: Hasan
-* Github: https://github.com/hasanrafi69
-* Email: hasanrafi69@gmail.com
-* Mobile: +880-161-782323
-*/
+ * Author: Hasan
+ * Github: https://github.com/hasanrafi69
+ * Email: hasanrafi69@gmail.com
+ * Mobile: +880-161-782323
+ */
+
+//Boostrap Carousel
+var myCarousel = document.querySelector('#myCarousel')
+var carousel = new bootstrap.Carousel(myCarousel, {
+    interval: 2000,
+    wrap: true
+})
+
+//Boostrap Toast
+var toastTrigger = document.getElementById('liveToastBtn')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+    toastTrigger.addEventListener('click', function() {
+        var toast = new bootstrap.Toast(toastLiveExample)
+
+        toast.show()
+    })
+}
